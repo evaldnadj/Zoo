c5d94c7fd39b5f7a0b09f295eb55cad8dbed5cab
evaldnadj <e.nadj@yahoo.com>
2018-01-09 21:57:32 +0100

    4. plus commit

---
+++ src/app/animal.ts
@@ -0,0 +1,13 @@
+
+export class Animal {
+    species: string;
+    name: string;
+    date_of_birth: string;
+
+    constructor(species: string, name: string, date_of_birth: string)
+    {
+        this.species=species;
+        this.name=name;
+        this.date_of_birth=date_of_birth;
+    }
+}
