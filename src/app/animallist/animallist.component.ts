a98d763d4af8858864c465a6d46f2f8907044d3c
evaldnadj <e.nadj@yahoo.com>
2018-01-09 20:27:03 +0100

    2. commit

---
+++ src/app/animallist/animallist.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animallist',
+  templateUrl: './animallist.component.html',
+  styleUrls: ['./animallist.component.css']
+})
+export class AnimallistComponent implements OnInit {
+  
+  animals: Array<Object>;
+
+  constructor() { 
+    this.animals = [
+     {
+      name: "Mara",
+      spices: "snake",
+      date_of_birth: "2017-12-12"
+     },
+     {
+      name: "Miki",
+      spices: "horse",
+      date_of_birth: "2016-12-01" 
+     },
+     {
+      name: "Milka",
+      spices: "cow",
+      date_of_birth: "2015-11-12"
+
+     },
+     {
+      name: "Mario",
+      spices: "wolf",
+      date_of_birth: "2013-09-12"
+
+     },
+     {
+      name: "Juk",
+      spices: "lion",
+      date_of_birth: "2011-05-12"
+     }
+    ]
+  }
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AnimallistComponent } from './animallist/animallist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimallistComponent
   ],
   imports: [
     BrowserModule
